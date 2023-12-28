7930ed35979bb9cd6dbca22e3e1a44b966a5c9b0
2961297295@qq.com <2961297295@qq.com>
2023-12-28 22:58:05 +0800

    1

---
+++ src/components/WholeProject.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <el-button type="primary" style="text-alien: center; font-size: 20px">主要按钮</el-button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {}
+  },
+  created() {},
+  methods: {},
+}
+</script>
+
+<style lang="less" scoped></style>
